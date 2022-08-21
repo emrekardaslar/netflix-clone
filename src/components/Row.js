1600cec5cc1d52a8ed456295742d23aad28d2b7b
emrekardaslar <emre.kardaslar@gmail.com>
2022-08-21 10:49:18 +0300

    init project & add Row component

---
+++ src/components/Row.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/w500";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              className="row__poster"
+              key={movie.id}
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Row;

52a4efc1a9f768362347d1e5764648a3f85760fb
emrekardaslar <emre.kardaslar@gmail.com>
2022-08-21 13:18:49 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
+import Nav from './components/Nav/Nav';
 import Row from './components/Row';
 import requests from './requests';
 
 function App() {
   return (
     <div className="app">
+     <Nav />
      <Banner />
      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
